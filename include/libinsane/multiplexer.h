b9cfe4bec50d5b64b21ee47fa247a8ce0364f0e3
Jerome Flesch <jflesch@openpaper.work>
2018-03-29 23:29:02 +0200

    Doc: Move API multiplexer doc to header file
    Signed-off-by: Jerome Flesch <jflesch@openpaper.work>

--- include/libinsane/multiplexer.h
+++ include/libinsane/multiplexer.h
@@ -8,9 +8,23 @@
 extern "C" {
 #endif
 
+/*!
+ * \brief API multiplexer
+ *
+ * LibInsane must be able to handle many APIs at once. For instance, on Windows,
+ * it must be able to use WIA2 (low-level and automation) and TWAIN, at the same
+ * time. So it includes an API multiplexer.
+ *
+ * The API multiplexer takes many API implementations in input, and merge them
+ * to present them as a single one.
+ *
+ * \param[in] input_implementations Implementations to wrap together.
+ * \param[in] nb_input_implementations Number of implementations to wrap together.
+ * \param[out] output_implementation Implementation wrapping all the input implementations.
+ */
 extern enum lis_error lis_api_multiplexer(
-	struct lis_api **input_apis, int nb_input_apis,
-	struct lis_api **output_api
+	struct lis_api **input_implementations, int nb_input_implementations,
+	struct lis_api **output_implementation
 );
 
 #ifdef __cplusplus
