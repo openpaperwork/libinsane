16b6a70d101c9dd9a745e4d354a3dc4f8baa0e6a
Jerome Flesch <jflesch@openpaper.work>
2018-04-24 09:18:48 +0200

    GObject: start adding code to instantiate Libinsane APIs (WIP)
    Signed-off-by: Jerome Flesch <jflesch@openpaper.work>

--- libinsane-gobject/include/libinsane-gobject/libinsane-api.h
+++ libinsane-gobject/include/libinsane-gobject/libinsane-api.h
@@ -1,23 +1,22 @@
 #ifndef __LIBINSANE_GOBJECT_H
 #define __LIBINSANE_GOBJECT_H
 
-#include <glib.h>
 #include <glib-object.h>
 
 G_BEGIN_DECLS
 
-#define LIBINSANE_API_TYPE                \
+#define LIBINSANE_API_TYPE \
      (libinsane_api_get_type())
-#define LIBINSANE_API(o)                  \
-    (G_TYPE_CHECK_INSTANCE_CAST ((o), LIBINSANE_API_TYPE, LibinsaneApi))
-#define LIBINSANE_CLASS(c)            \
-    (G_TYPE_CHECK_CLASS_CAST ((c), LIBINSANE_API_TYPE, LibinsaneApiClass))
-#define LIBINSANE_IS_API(o)               \
-    (G_TYPE_CHECK_INSTANCE_TYPE ((o), LIBINSANE_API_TYPE))
-#define LIBINSANE_IS_API_CLASS(c)         \
-    (G_TYPE_CHECK_CLASS_TYPE ((c),  LIBINSANE_API_TYPE))
-#define LIBINSANE_API_GET_CLASS(o)        \
-    (G_TYPE_INSTANCE_GET_CLASS ((o), LIBINSANE_API_TYPE, LibinsaneApiClass))
+#define LIBINSANE_API(obj) \
+    (G_TYPE_CHECK_INSTANCE_CAST ((obj), LIBINSANE_API_TYPE, LibinsaneApi))
+#define LIBINSANE_CLASS(cls) \
+    (G_TYPE_CHECK_CLASS_CAST ((cls), LIBINSANE_API_TYPE, LibinsaneApiClass))
+#define LIBINSANE_IS_API(obj) \
+    (G_TYPE_CHECK_INSTANCE_TYPE ((obj), LIBINSANE_API_TYPE))
+#define LIBINSANE_IS_API_CLASS(cls) \
+    (G_TYPE_CHECK_CLASS_TYPE ((cls),  LIBINSANE_API_TYPE))
+#define LIBINSANE_API_GET_CLASS(obj) \
+    (G_TYPE_INSTANCE_GET_CLASS ((obj), LIBINSANE_API_TYPE, LibinsaneApiClass))
 
 typedef struct _LibinsaneApi LibinsaneApi;
 typedef struct _LibinsaneApiPrivate LibinsaneApiPrivate;
@@ -32,6 +31,15 @@ struct _LibinsaneApiClass {
 };
 
 GType libinsane_api_get_type(void) G_GNUC_CONST;
+
+/**
+ * libinsane_api_new_from_string:
+ * Instantiate a new list of API based on a provided string.
+ * See \ref lis_str2impls
+ *
+ * Returns: (transfer full): the new reference to @self.
+ */
+LibinsaneApi *libinsane_api_new_from_string(const char *desc);
 
 G_END_DECLS
 
