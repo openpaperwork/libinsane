e19b8c34c5830abfce09d4b3d958b2800acb06fd
Jerome Flesch <jflesch@openpaper.work>
2018-05-10 00:56:44 +0200

    Libinsane: implements lis_strerror()
    Signed-off-by: Jerome Flesch <jflesch@openpaper.work>

---
+++ libinsane/src/error.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+
+#include <libinsane/error.h>
+
+const char *lis_strerror(enum lis_error err)
+{
+	switch(err) {
+		case LIS_OK:
+			return "Success";
+		case LIS_APPROXIMATE_VALUE:
+			return "Value approximated";
+		case LIS_CANCELLED:
+			return "Operation cancelled";
+		case LIS_END_OF_PAGE:
+			return "End of page";
+		case LIS_END_OF_FEED:
+			return "End of feed";
+		case LIS_WARMING_UP:
+			return "Device is warming up";
+		case LIS_ERR_DEVICE_BUSY:
+			return "Device busy";
+		case LIS_ERR_UNSUPPORTED:
+			return "Operation not supported";
+		case LIS_ERR_INVALID_VALUE:
+			return "Invalid value";
+		case LIS_ERR_JAMMED:
+			return "Device jammed";
+		case LIS_ERR_COVER_OPEN:
+			return "Device cover is opened";
+		case LIS_ERR_IO_ERROR:
+			return "I/O Error";
+		case LIS_ERR_NO_MEM:
+			return "Out of memory";
+		case LIS_ERR_ACCESS_DENIED:
+			return "Access denied";
+		case LIS_ERR_HW_IS_LOCKED:
+			return "Hardware is locked (used by another application ?)";
+		case LIS_ERR_INTERNAL_IMG_FORMAT_NOT_SUPPORTED:
+			return "LibInsane internal error: Image format not supported (please report !)";
+		case LIS_ERR_INTERNAL_NOT_IMPLEMENTED:
+			return "LibInsane internal error: Operation not implemented (please report !)";
+	}
+	return NULL;
+}
