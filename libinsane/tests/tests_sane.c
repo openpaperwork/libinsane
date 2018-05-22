38228ba63e3216516b7565c70bd4ff30271e5202
Jerome Flesch <jflesch@openpaper.work>
2018-05-22 23:41:50 +0200

    add tests on sane wrapper
    Signed-off-by: Jerome Flesch <jflesch@openpaper.work>

---
+++ libinsane/tests/tests_sane.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+
+#include <CUnit/Basic.h>
+
+#include <libinsane/capi.h>
+#include <libinsane/dumb.h>
+#include <libinsane/sane.h>
+#include <libinsane/util.h>
+
+#define TEST_DEV_ID "test:0"
+
+
+static struct lis_api *g_sane;
+// static struct lis_item g_test_device;
+
+static int tests_sane_init(void)
+{
+	enum lis_error err;
+
+	err = lis_api_sane(&g_sane);
+	if (LIS_IS_ERROR(err)) {
+		fprintf(stderr, "Failed to init Sane: 0x%X, %s\n", err, lis_strerror(err));
+		return -1;
+	}
+
+	return 0;
+}
+
+static int tests_sane_clean(void)
+{
+	g_sane->cleanup(g_sane);
+	return 0;
+}
+
+static void tests_sane_list_devices(void)
+{
+	struct lis_device_descriptor **devs;
+	enum lis_error err;
+	int i;
+	int has_test0 = 0;
+
+	err = g_sane->list_devices(g_sane, LIS_DEVICE_LOCATIONS_LOCAL_ONLY, &devs);
+	CU_ASSERT_TRUE(LIS_IS_OK(err));
+
+	for (i = 0 ; devs[i] != NULL ; i++) {
+		CU_ASSERT_NOT_EQUAL(devs[i]->dev_id, NULL);
+		if (devs[i]->dev_id != NULL && strcmp(devs[i]->dev_id, TEST_DEV_ID) == 0) {
+			has_test0 = 1;
+		}
+		CU_ASSERT_NOT_EQUAL(devs[i]->vendor, NULL);
+		CU_ASSERT_NOT_EQUAL(devs[i]->model, NULL);
+		CU_ASSERT_NOT_EQUAL(devs[i]->type, NULL);
+	}
+	CU_ASSERT_TRUE(has_test0);
+}
+
+int register_tests(void)
+{
+	CU_pSuite suite = NULL;
+
+	suite = CU_add_suite("Sane", tests_sane_init, tests_sane_clean);
+	if (suite == NULL) {
+		fprintf(stderr, "CU_add_suite() failed\n");
+		return 0;
+	}
+
+	if (CU_add_test(suite, "list_devices()", tests_sane_list_devices) == NULL) {
+		fprintf(stderr, "CU_add_test() has failed\n");
+		return 0;
+	}
+
+	return 1;
+}
